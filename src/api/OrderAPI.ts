a6fff829995035f069bb40aa44b8d89a3e5efcdc
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2026-08-16 18:00:54 -0400

    * Greatly optimized api/* directory files by applying DRY principles and reducing debug code along with other minor changes.     * axios.ts:         - Extracted error handling logic present in each API file method into a separate requestErrorHandler function         that is now imported and used in each API file method.         - "api" is no more the default exporf of axios.ts, instead api and requestErrorHandler are now named exports.     * AdminAPI.ts:         - Removed debug code and console logs.         - Removed redundant code and applied DRY principles by implementing requestErrorHandler in catch statements.     * AuthAPI.ts:         - Removed debug code and console logs.         - Removed redundant code and applied DRY principles by implementing requestErrorHandler in catch statements.     * BsaleOrderAPI.ts:         - Removed debug code and console logs.         - Removed redundant code and applied DRY principles by implementing requestErrorHandler in catch statements.     * OrderAPI.ts:         - Removed debug code and console logs.         - Removed redundant code and applied DRY principles by implementing requestErrorHandler in catch statements.     * ProductAPI.ts:         - Removed debug code and console logs.         - Removed redundant code and applied DRY principles by implementing requestErrorHandler in catch statements.     * ProfileAPI.ts:         - Removed debug code and console logs.         - Removed redundant code and applied DRY principles by implementing requestErrorHandler in catch statements.     * utilities/copy.tsx now is utilities/copy.ts I don't know why it was set as .tsx     * tsconfig.app.json: added IgnoreDeprecations 6.0 to avoid error warnings.

--- src/api/OrderAPI.ts
+++ src/api/OrderAPI.ts
@@ -1,249 +1,186 @@
-import api from "@/lib/axios";
-import { isAxiosError } from "axios";
-import { getOrdersResponseSchema, Order, OrderForm, orderSchema, OrderStatusForm, updateOrderResponseSchema } from "../types";
+import {
+	getOrdersResponseSchema,
+	Order,
+	OrderForm,
+	orderSchema,
+	OrderStatusForm,
+	updateOrderResponseSchema,
+} from "../types";
+import { api, requestErrorHandler } from "@/lib/axios";
 
-//TODO: Work on API Calls to the /orders endpoints
+export async function getOrdersAdmin({
+	page,
+	perPage,
+	status,
+	businessId,
+	country,
+}: {
+	page: number;
+	perPage: number;
+	status: string;
+	businessId: string;
+	country: string;
+}) {
+	try {
+		// Base url
+		let url = `/orders?page=${page}&perPage=${perPage}`;
 
-export async function getOrdersAdmin({ page, perPage, status, businessId, country } : { page: number, perPage: number, status: string, businessId: string, country: string }) {
-    try {
-        // Base url
-        let url = `/orders?page=${page}&perPage=${perPage}`;
+		// Conditionally add filters to search url if they exists and are not empty
+		if (country) {
+			url += `&country=${encodeURIComponent(country)}`;
+		}
 
-        // Conditionally add filters to search url if they exists and are not empty
-        if(country) {
-            url += `&country=${encodeURIComponent(country)}`
-        }
-        
-        if(status) {
-            url += `&status=${encodeURIComponent(status)}`
-        }
+		if (status) {
+			url += `&status=${encodeURIComponent(status)}`;
+		}
 
-        if(businessId) {
-            url += `&businessId=${encodeURIComponent(businessId)}`
-        }
+		if (businessId) {
+			url += `&businessId=${encodeURIComponent(businessId)}`;
+		}
 
-        const { data } = await api.get(url);
-        
-        const response = getOrdersResponseSchema.safeParse(data);
-        if (!response.success) {
-            console.error("❌ Zod validation failed:", response.error);
-            throw new Error("Invalid API response format");
-        }
+		const { data } = await api.get(url);
 
-        return response.success ? response.data : null;
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
+		const response = getOrdersResponseSchema.safeParse(data);
+		if (!response.success) {
+			console.error("❌ Zod validation failed:", response.error);
+			throw new Error("Invalid API response format");
+		}
 
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente.");
-        }
-    }
+		return response.success ? response.data : null;
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
 export async function getOrderByIdAdmin(orderId: string) {
-    if (!orderId) return null; // Prevent undefined
-    try {
-        const { data } = await api.get(`/orders/${orderId}`);
+	if (!orderId) return null; // Prevent undefined
 
-        const result = orderSchema.safeParse(data);
-        if (!result.success) {
-            console.error("❌ Zod validation error:", result.error);
-            return null; // <-- ALWAYS return something
-        }
+	try {
+		const { data } = await api.get(`/orders/${orderId}`);
 
-        return result.data; // Parsed & valid                 
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
+		const result = orderSchema.safeParse(data);
+		if (!result.success) {
+			console.error("❌ Zod validation error:", result.error);
+			return null; // <-- ALWAYS return something
+		}
 
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente. Si el error persiste, contacta al administrador.");
-        }
-    }
+		return result.data; // Parsed & valid
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
-export async function getOrdersUser({ page, perPage, status, country, orderId } : { page: number, perPage: number, status: string, country: string, orderId: string }) {
-    try {
-        // Base url
-        let url = `/orders/user?page=${page}&perPage=${perPage}`;
+export async function getOrdersUser({
+	page,
+	perPage,
+	status,
+	country,
+	orderId,
+}: {
+	page: number;
+	perPage: number;
+	status: string;
+	country: string;
+	orderId: string;
+}) {
+	try {
+		// Base url
+		let url = `/orders/user?page=${page}&perPage=${perPage}`;
 
-        // Conditionally add filters to search url if they exists and are not empty
-        if(country) {
-            url += `&country=${encodeURIComponent(country)}`
-        }
-        
-        if(status) {
-            url += `&status=${encodeURIComponent(status)}`
-        }
+		// Conditionally add filters to search url if they exists and are not empty
+		if (country) {
+			url += `&country=${encodeURIComponent(country)}`;
+		}
 
-        if(orderId) {
-            url += `&orderId=${encodeURIComponent(orderId)}`
-        }
+		if (status) {
+			url += `&status=${encodeURIComponent(status)}`;
+		}
 
-        const { data } = await api.get(url);
+		if (orderId) {
+			url += `&orderId=${encodeURIComponent(orderId)}`;
+		}
 
-        const response = getOrdersResponseSchema.safeParse(data);
+		const { data } = await api.get(url);
 
-        return response.data;
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
+		const response = getOrdersResponseSchema.safeParse(data);
 
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente.");
-        }
-    }
+		return response.data;
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
-export async function getOrderByIdUser(orderId: Order["_id"]) {
-    try {
-        const url = `/orders/user/${orderId}`;
-        const { data } = await api.get(url);
+export async function getOrderByIdUser(orderId: Order["id"]) {
+	try {
+		const url = `/orders/user/${orderId}`;
+		const { data } = await api.get(url);
 
-        const response = orderSchema.safeParse(data);
+		const response = orderSchema.safeParse(data);
 
-        return response.data;                  
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
-
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente. Si el error persiste, contacta al administrador.");
-        }
-    }
+		return response.data;
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
 export async function createOrder(formData: OrderForm) {
-    try {
-        const url = `/orders`;
-        const { data } = await api.post(url, formData);
+	try {
+		const url = `/orders`;
+		const { data } = await api.post(url, formData);
 
-        const response = orderSchema.safeParse(data);
+		const response = orderSchema.safeParse(data);
 
-        return response.data;                  
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
-
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente. Si el error persiste, contacta al administrador.");
-        }
-    }
+		return response.data;
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
-export async function updateOrder({ orderId, formData } : {orderId: string, formData: OrderForm }) {
-    try {
-        const url = `/orders/${orderId}`;
-        const { data } = await api.patch(url, formData);
+export async function updateOrder({
+	orderId,
+	formData,
+}: {
+	orderId: string;
+	formData: OrderForm;
+}) {
+	try {
+		const url = `/orders/${orderId}`;
+		const { data } = await api.patch(url, formData);
 
-        const response = updateOrderResponseSchema.safeParse(data);
+		const response = updateOrderResponseSchema.safeParse(data);
 
-        return response.data;                  
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
-
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente. Si el error persiste, contacta al administrador.");
-        }
-    }
+		return response.data;
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
-export async function updateOrderStatus({ orderId, formData } : {orderId: string, formData: OrderStatusForm }) {
-    try {
-        const url = `/orders/status/${orderId}`;
-        const { data } = await api.patch(url, formData);
+export async function updateOrderStatus({
+	orderId,
+	formData,
+}: {
+	orderId: string;
+	formData: OrderStatusForm;
+}) {
+	try {
+		const url = `/orders/status/${orderId}`;
+		const { data } = await api.patch(url, formData);
 
-        const response = updateOrderResponseSchema.safeParse(data);
+		const response = updateOrderResponseSchema.safeParse(data);
 
-        return response.data;                  
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
-
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente. Si el error persiste, contacta al administrador.");
-        }
-    }
+		return response.data;
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
 export async function deleteOrder(orderId: string) {
-    try {
-        const url = `/orders/${orderId}`;
-        const response = await api.delete(url);
+	try {
+		const url = `/orders/${orderId}`;
+		const response = await api.delete(url);
 
-        return response.data;                  
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
-
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente. Si el error persiste, contacta al administrador.");
-        }
-    }
+		return response.data;
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
--- src/api/ProductAPI.ts
+++ src/api/ProductAPI.ts
@@ -1,162 +1,124 @@
 import api from "@/lib/bsale";
-import { isAxiosError } from "axios";
-import { CartForm, detailCartSchema, productWebDescriptionResponseSchema } from "../types";
+import {
+	CartForm,
+	detailCartSchema,
+	productWebDescriptionResponseSchema,
+} from "../types";
+import { requestErrorHandler } from "@/lib/axios";
 
 // Conseguir Descripción Web del Producto
-export async function getAllProductDescription({limit, offset, name, code} : {limit: number, offset: number, name?: string, code?: string}) {
-    try {
-        //const url = `/v2/products/list/market_info.json?limit=${limit}&offset=${offset}&expand=[descriptions, variantsInfo, variant.salePrice, variant.stock, productType, images, baseInfo, variant.discount, brand]&priceListId=1&name=${name}`;
+export async function getAllProductDescription({
+	limit,
+	offset,
+	name,
+	code,
+}: {
+	limit: number;
+	offset: number;
+	name?: string;
+	code?: string;
+}) {
+	try {
+		//const url = `/v2/products/list/market_info.json?limit=${limit}&offset=${offset}&expand=[descriptions, variantsInfo, variant.salePrice, variant.stock, productType, images, baseInfo, variant.discount, brand]&priceListId=1&name=${name}`;
 
-        // Base URL
-        let url = `/v2/products/list/market_info.json?limit=${limit}&offset=${offset}&expand=[descriptions, variantsInfo, variant.salePrice, variant.stock, productType, images, baseInfo, variant.discount, brand]&priceListId=1&productWebType=virtual`;
+		// Base URL
+		let url = `/v2/products/list/market_info.json?limit=${limit}&offset=${offset}&expand=[descriptions, variantsInfo, variant.salePrice, variant.stock, productType, images, baseInfo, variant.discount, brand]&priceListId=1&productWebType=virtual`;
 
-        // Conditionally add name if it exists
-        if (name) {
-            url += `&name=${encodeURIComponent(name)}`;
-        }
+		// Conditionally add name if it exists
+		if (name) {
+			url += `&name=${encodeURIComponent(name)}`;
+		}
 
-        // Conditionally add code if it exists and is not empty
-        if (code) {
-            url += `&code=${encodeURIComponent(code)}`;
-        }
+		// Conditionally add code if it exists and is not empty
+		if (code) {
+			url += `&code=${encodeURIComponent(code)}`;
+		}
 
-        //console.log(url)
-        const { data } = await api.get(url);
-        //console.log(data)
-        
-        const response = productWebDescriptionResponseSchema.safeParse(data);
-        if(response.success) {
-            //console.log("✅ Respuesta exitosa de la API:", response.data);
-            return response.data;
-        }
+		//console.log(url)
+		const { data } = await api.get(url);
+		//console.log(data)
 
-        console.error("Schema Validation Failed", response.error);
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
+		const response = productWebDescriptionResponseSchema.safeParse(data);
+		if (response.success) {
+			//console.log("✅ Respuesta exitosa de la API:", response.data);
+			return response.data;
+		}
 
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente.");
-        }
-    }
+		console.error("Schema Validation Failed", response.error);
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
 //? Get Cart Detail by ID
-export async function getCartDetailById(cartId : number) {
-    try {
-        const url = `/v1/cart/${cartId}/detail.json`;
-        const { data } = await api.get(url);
-        //console.log(data)
+export async function getCartDetailById(cartId: number) {
+	try {
+		const url = `/v1/cart/${cartId}/detail.json`;
+		const { data } = await api.get(url);
+		//console.log(data)
 
-        const response = detailCartSchema.safeParse(data);
-        if(response.success) {
-            ////console.log("✅ Respuesta exitosa de la API:", response.data);
-            return response.data;
-        }
+		const response = detailCartSchema.safeParse(data);
+		if (response.success) {
+			////console.log("✅ Respuesta exitosa de la API:", response.data);
+			return response.data;
+		}
 
-        console.error("Schema Validation Failed", response.error);
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
-
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente a.");
-        }
-    }  
+		console.error("Schema Validation Failed", response.error);
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
-//? Add to Cart 
-export async function addToCart(formData : CartForm) {
-    try {
-        const url = `/v1/cart/new.json`;
-        const response = await api.post(url, formData);
+//? Add to Cart
+export async function addToCart(formData: CartForm) {
+	try {
+		const url = `/v1/cart/new.json`;
+		const response = await api.post(url, formData);
 
-        if(response.data.code === "200") {
-            const cartId = response.data.data.id;
-            localStorage.setItem("cartId", cartId.toString());
-            return cartId;
-        }
+		if (response.data.code === "200") {
+			const cartId = response.data.data.id;
+			localStorage.setItem("cartId", cartId.toString());
+			return cartId;
+		}
 
-        console.error("❌ Error en la solicitud:", response.data);
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
-
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente.");
-        }
-    }
+		console.error("❌ Error en la solicitud:", response.data);
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
 //? Update Cart
-export async function updateCart({formData, cartId} : {formData: CartForm, cartId: number}) {
-    try {
-        const url = `/v1/cart/${cartId}.json`;
-        const response = await api.put(url, formData);
+export async function updateCart({
+	formData,
+	cartId,
+}: {
+	formData: CartForm;
+	cartId: number;
+}) {
+	try {
+		const url = `/v1/cart/${cartId}.json`;
+		const response = await api.put(url, formData);
 
-        return response.data;
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
-
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            // Lanzamos un error más detallado para que pueda ser manejado correctamente
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente.");
-        }
-    }
+		return response.data;
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
 
 //? Delete Cart
-export async function deleteCart({ cartId, detailId }: { cartId: number; detailId: number }) {
-    try {
-        const url = `/v1/cart/${cartId}/detail/${detailId}.json`;
-        const response = await api.delete(url);
+export async function deleteCart({
+	cartId,
+	detailId,
+}: {
+	cartId: number;
+	detailId: number;
+}) {
+	try {
+		const url = `/v1/cart/${cartId}/detail/${detailId}.json`;
+		const response = await api.delete(url);
 
-        return response.data;
-    } catch (error) {
-        console.error("❌ Error en la solicitud:", error);
-
-        if (isAxiosError(error)) {
-            console.error("🔍 Error de Axios detectado:");
-            console.error("➡️ Código de estado:", error.response?.status);
-            console.error("➡️ Mensaje de error:", error.response?.data?.error || error.message);
-            console.error("➡️ Respuesta completa:", error.response?.data);
-
-            throw new Error(error.response?.data?.message || "Ocurrió un error en la API");
-        } else {
-            console.error("⚠️ Error desconocido:", error);
-            throw new Error("Error inesperado. Intenta nuevamente.");
-        }
-    }
+		return response.data;
+	} catch (error) {
+		return requestErrorHandler(error as Error);
+	}
 }
