696e2612d92f830a0c8bb2a28200481f4d684cfe
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2026-08-19 16:51:24 -0400

    * Updated some of the admin order components to account for new API structures for peru integration.     * CreateOrderForm.tsx:         - Added form fields for new API structure for peru integration:             - purchaseOrderNumber             - currency (automatically set when selecting country in the country select input)             - changed payment attribute references to paymentMethod to match new API structure.             - Changed the structure of the form a little. Moved order details section to the middle and             products and costs overview to the bottom.         - Now uses new formatCurrency function instead of fixed formatToCLP function.     * EditOrderForm.tsx:         - Same updates as CreateOrderForm.tsx for the new API structure for peru integration, except         they are now adapted fo the update operation.     * AdminOrdersView.tsx:         - Fixed typo in search by country search bar base URL.         Was pointing towards client order search instead of admin.         - Added countriesOptions object to be used in the search by country search bar select dropdown.         - Now search by country search bar is a select dropdown instead of a text input,         similiar to it's neighboring search by order status search bar component.     * OrdersView.tsx:         - Same updates as AdminOrdersView.tsx for the search by country search bar component, except         for the typo fix.

--- src/views/admin/orders/AdminOrderView.tsx
+++ src/views/admin/orders/AdminOrderView.tsx
@@ -5,245 +5,279 @@ import Loader from "@/components/ui/Loader";
 import Pagination from "@/components/ui/Pagination";
 import SearchBar from "@/components/ui/SearchBar";
 import { useQuery } from "@tanstack/react-query";
-import { CheckCircle, Package, Pen, Truck, XCircle } from "lucide-react";
+import { CheckCircle, Earth, Package, Pen, Truck, XCircle } from "lucide-react";
 import { useEffect } from "react";
 import { Link, Navigate, useSearchParams } from "react-router-dom";
 
 const orderStatusOptions = [
-    {
-        value: "Pendiente",
-        label: "Pendiente",
-        icon: Package,
-        color: "text-yellow-600"
-    },
-    {
-        value: "En Transito",
-        label: "En Tránsito",
-        icon: Truck,
-        color: "text-blue-600"
-    },
-    {
-        value: "Entregado",
-        label: "Entregado",
-        icon: CheckCircle,
-        color: "text-green-600"
-    },
-    {
-        value: "Cancelado",
-        label: "Cancelado",
-        icon: XCircle,
-        color: "text-red-600"
-    }
+	{
+		value: "Pendiente",
+		label: "Pendiente",
+		icon: Package,
+		color: "text-yellow-600",
+	},
+	{
+		value: "En Transito",
+		label: "En Tránsito",
+		icon: Truck,
+		color: "text-blue-600",
+	},
+	{
+		value: "Entregado",
+		label: "Entregado",
+		icon: CheckCircle,
+		color: "text-green-600",
+	},
+	{
+		value: "Cancelado",
+		label: "Cancelado",
+		icon: XCircle,
+		color: "text-red-600",
+	},
+];
+
+const countriesOptions = [
+	{
+		value: "Chile",
+		label: "Chile",
+		icon: Earth,
+		color: "text-blue-500",
+	},
+	{
+		value: "Perú",
+		label: "Perú",
+		icon: Earth,
+		color: "text-red-500",
+	},
 ];
 
 export default function AdminOrdersView() {
-    const [searchParams, setSearchParams] = useSearchParams();
-    
-    // Get search parameters
-    const searchCountry = searchParams.get("country") || "";
-    const searchStatus = searchParams.get("status") || "";
-    const searchBusinessId = searchParams.get("businessId") || "";
-    const page = parseInt(searchParams.get("page") || "1", 10);
+	const [searchParams, setSearchParams] = useSearchParams();
 
-    const itemsPerPage = 10;
+	// Get search parameters
+	const searchCountry = searchParams.get("country") || "";
+	const searchStatus = searchParams.get("status") || "";
+	const searchBusinessId = searchParams.get("businessId") || "";
+	const page = parseInt(searchParams.get("page") || "1", 10);
 
-    // Fetch orders with filters
-    const { data, isLoading, isError } = useQuery({
-        queryKey: ["orders", searchCountry, searchBusinessId, searchStatus, page],
-        queryFn: () => getOrdersAdmin({
-            perPage: itemsPerPage, 
-            page, 
-            country: searchCountry, 
-            status: searchStatus, 
-            businessId: searchBusinessId
-        }),
-        staleTime: 1000 * 60 * 5,
-        refetchOnWindowFocus: false,
-    });
+	const itemsPerPage = 15;
 
-    // Reset to page 1 when toggling filter or changing search params
-    useEffect(() => {
-        if (page !== 1) {
-            const newParams = new URLSearchParams(searchParams);
-            newParams.set("page", "1");
-            setSearchParams(newParams);
-        }
-    }, [searchCountry, searchStatus, searchBusinessId]);
-    
-    if (page < 1) return <Navigate to={`/admin/orders?page=1`} replace />;
+	// Fetch orders with filters
+	const { data, isLoading, isError } = useQuery({
+		queryKey: [
+			"orders",
+			searchCountry,
+			searchBusinessId,
+			searchStatus,
+			page,
+		],
+		queryFn: () =>
+			getOrdersAdmin({
+				perPage: itemsPerPage,
+				page,
+				country: searchCountry,
+				status: searchStatus,
+				businessId: searchBusinessId,
+			}),
+		staleTime: 1000 * 60 * 5,
+		refetchOnWindowFocus: false,
+	});
 
-    if (isLoading) return <Loader />;
+	// Reset to page 1 when toggling filter or changing search params
+	useEffect(() => {
+		if (page !== 1) {
+			const newParams = new URLSearchParams(searchParams);
+			newParams.set("page", "1");
+			setSearchParams(newParams);
+		}
+	}, [searchCountry, searchStatus, searchBusinessId]);
 
-    if (isError) return <Navigate to="/404" replace />;
+	if (page < 1) return <Navigate to={`/admin/orders?page=1`} replace />;
 
-    // Get data from API response
-    const orders = data?.orders || [];
-    const totalOrders = data?.totalOrders || 0;
-    const totalPages = data?.totalPages || 1;
+	if (isLoading) return <Loader />;
 
-    // Check if there are any active searches
-    const hasActiveSearch = searchCountry || searchStatus || searchBusinessId;
+	if (isError) return <Navigate to="/404" replace />;
 
-    if (orders.length === 0) return (
-        <>
-            <Heading>
-                {hasActiveSearch ? "Orden no Encontrada" : "Administrar Órdenes"}
-            </Heading>
+	// Get data from API response
+	const orders = data?.orders || [];
+	const totalOrders = data?.totalOrders || 0;
+	const totalPages = data?.totalPages || 1;
 
-            {!hasActiveSearch && (
-                <p className="text-gray-700 text-center my-10">
-                    Aquí puedes ver los Detalles de las Órdenes y <span className="font-bold text-orange-500">Administrarlas.</span>
-                </p>
-            )}
+	// Check if there are any active searches
+	const hasActiveSearch = searchCountry || searchStatus || searchBusinessId;
 
-            <SearchBar
-                route="orders"
-                param="orderId"
-                inputType="text"
-                formText="Buscar Orden por ID "
-                mini
-                searchText="Orden"
-            />
-            
-            {hasActiveSearch && (
-                <>
-                    <SearchBar
-                        route="admin/orders"
-                        param="businessId"
-                        inputType="text"
-                        formText="Buscar Orden por ID de la Empresa"
-                        searchText="Empresa"
-                    />
-        
-                    <SearchBar
-                        route="admin/orders"
-                        param="country"
-                        inputType="text"
-                        formText="Buscar Orden por País. Ej. Chile"
-                        searchText="País"
-                    />
-        
-                    <SearchBar
-                        route="admin/orders"
-                        param="status"
-                        inputType="select"
-                        formText="Buscar Orden por su Estado. Ej. Pendiente"
-                        searchText="Estado"
-                        options={orderStatusOptions}
-                    />
-                </>
-            )}
+	if (orders.length === 0)
+		return (
+			<>
+				<Heading>
+					{hasActiveSearch
+						? "Orden no Encontrada"
+						: "Administrar Órdenes"}
+				</Heading>
 
-            <div className="flex justify-center mt-8">
-                <Link
-                    to={"/admin/orders/create"}
-                    className="flex items-center gap-2 rounded-md bg-orange-600 px-8 py-3 text-sm text-center font-bold text-white shadow-xs transition-colors duration-200 hover:bg-orange-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
-                >
-                    <Pen size={16} />
-                    Registrar Orden
-                </Link>
-            </div>
+				{!hasActiveSearch && (
+					<p className="text-gray-700 text-center my-10">
+						Aquí puedes ver los Detalles de las Órdenes y{" "}
+						<span className="font-bold text-orange-500">
+							Administrarlas.
+						</span>
+					</p>
+				)}
 
-            {hasActiveSearch ? (
-                <p className="text-gray-700 text-center my-10">
-                    No se encontraron órdenes con los filtros aplicados
-                </p>
-            ) : (
-                <p className="text-gray-700 text-center my-10 border-b border-gray-300 pb-5">
-                    No hay órdenes registradas. Realiza una búsqueda o comienza a registrar nuevas ordenes.
-                </p>
-            )}
+				<SearchBar
+					route="admin/orders"
+					param="orderId"
+					inputType="text"
+					formText="Buscar Orden por ID "
+					mini
+					searchText="Orden"
+				/>
 
-            {hasActiveSearch && (
-                <div className="flex gap-5 justify-center my-10">
-                    <Link
-                        className="bg-orange-500 hover:bg-orange-600 text-white px-8 py-2 rounded-full transition-colors"
-                        to="/admin/orders"
-                    >
-                        Volver a Todas las Órdenes
-                    </Link>
-                </div>
-            )}
-        </>
-    );
+				{hasActiveSearch && (
+					<>
+						<SearchBar
+							route="admin/orders"
+							param="businessId"
+							inputType="text"
+							formText="Buscar Orden por ID de la Empresa"
+							searchText="Empresa"
+						/>
 
-    return (
-        <>
-            <Heading>Administrar Órdenes</Heading>
+						<SearchBar
+							route="admin/orders"
+							param="country"
+							inputType="select"
+							formText="Buscar Orden por País. Ej. Chile"
+							searchText="País"
+							options={countriesOptions}
+						/>
 
-            <p className="text-gray-700 text-center mt-10">
-                Aquí puedes administrar las órdenes de tus clientes
-            </p>
+						<SearchBar
+							route="admin/orders"
+							param="status"
+							inputType="select"
+							formText="Buscar Orden por su Estado. Ej. Pendiente"
+							searchText="Estado"
+							options={orderStatusOptions}
+						/>
+					</>
+				)}
 
-            <SearchBar
-                route="orders"
-                param="orderId"
-                inputType="text"
-                formText="Buscar Orden por ID "
-                mini
-                searchText="Orden"
-            />
+				<div className="flex justify-center mt-8">
+					<Link
+						to={"/admin/orders/create"}
+						className="flex items-center gap-2 rounded-md bg-orange-600 px-8 py-3 text-sm text-center font-bold text-white shadow-xs transition-colors duration-200 hover:bg-orange-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+					>
+						<Pen size={16} />
+						Registrar Orden
+					</Link>
+				</div>
 
-            <SearchBar
-                route="admin/orders"
-                param="businessId"
-                inputType="text"
-                formText="Buscar Orden por ID de la Empresa"
-                searchText="Empresa"
-            />
+				{hasActiveSearch ? (
+					<p className="text-gray-700 text-center my-10">
+						No se encontraron órdenes con los filtros aplicados
+					</p>
+				) : (
+					<p className="text-gray-700 text-center my-10 border-b border-gray-300 pb-5">
+						No hay órdenes registradas. Realiza una búsqueda o
+						comienza a registrar nuevas ordenes.
+					</p>
+				)}
 
-            <SearchBar
-                route="admin/orders"
-                param="country"
-                inputType="text"
-                formText="Buscar Orden por País. Ej. Chile"
-                searchText="País"
-            />
+				{hasActiveSearch && (
+					<div className="flex gap-5 justify-center my-10">
+						<Link
+							className="bg-orange-500 hover:bg-orange-600 text-white px-8 py-2 rounded-full transition-colors"
+							to="/admin/orders"
+						>
+							Volver a Todas las Órdenes
+						</Link>
+					</div>
+				)}
+			</>
+		);
 
-            <SearchBar
-                route="admin/orders"
-                param="status"
-                inputType="select"
-                formText="Buscar Orden por su Estado. Ej. Pendiente"
-                searchText="Estado"
-                options={orderStatusOptions}
-            />
+	return (
+		<>
+			<Heading>Administrar Órdenes</Heading>
 
-            <p className="text-gray-700 text-center my-10">
-                Al cambiar el estado de la orden, el cliente <span className="font-bold text-orange-500">será notificado por correo</span>
-            </p>
+			<p className="text-gray-700 text-center mt-10">
+				Aquí puedes administrar las órdenes de tus clientes
+			</p>
 
-            {hasActiveSearch && (
-                <>
-                    <div className="text-center mt-10 mb-10">
-                        <Link
-                            className="bg-orange-500 hover:bg-orange-600 text-white px-8 py-2 rounded-full transition-colors"
-                            to="/admin/orders"
-                        >
-                            Volver a Todas las Órdenes
-                        </Link>
-                    </div>
+			<SearchBar
+				route="admin/orders"
+				param="orderId"
+				inputType="text"
+				formText="Buscar Orden por ID "
+				mini
+				searchText="Orden"
+			/>
 
-                    <p className="text-gray-700 text-center mt-10">
-                        Órdenes encontradas: <span className="font-bold text-orange-500">{totalOrders}</span>
-                    </p>
-                </>
-            )}
+			<SearchBar
+				route="admin/orders"
+				param="businessId"
+				inputType="text"
+				formText="Buscar Orden por ID de la Empresa"
+				searchText="Empresa"
+			/>
 
-            <div className="my-[32px] max-w-7xl xl:max-w-full mx-auto">
-                <OrderTable
-                    admin={true}
-                    orders={orders}
-                />
-            </div>
+			<SearchBar
+				route="admin/orders"
+				param="country"
+				inputType="select"
+				formText="Buscar Orden por País. Ej. Chile"
+				searchText="País"
+				options={countriesOptions}
+			/>
 
-            {totalPages > 1 && (
-                <Pagination
-                    route="admin/orders"
-                    page={page}
-                    totalPages={totalPages}
-                />
-            )}
-        </>
-    );
+			<SearchBar
+				route="admin/orders"
+				param="status"
+				inputType="select"
+				formText="Buscar Orden por su Estado. Ej. Pendiente"
+				searchText="Estado"
+				options={orderStatusOptions}
+			/>
+
+			<p className="text-gray-700 text-center my-10">
+				Al cambiar el estado de la orden, el cliente{" "}
+				<span className="font-bold text-orange-500">
+					será notificado por correo
+				</span>
+			</p>
+
+			{hasActiveSearch && (
+				<>
+					<div className="text-center mt-10 mb-10">
+						<Link
+							className="bg-orange-500 hover:bg-orange-600 text-white px-8 py-2 rounded-full transition-colors"
+							to="/admin/orders"
+						>
+							Volver a Todas las Órdenes
+						</Link>
+					</div>
+
+					<p className="text-gray-700 text-center mt-10">
+						Órdenes encontradas:{" "}
+						<span className="font-bold text-orange-500">
+							{totalOrders}
+						</span>
+					</p>
+				</>
+			)}
+
+			<div className="my-[32px] max-w-7xl xl:max-w-full mx-auto">
+				<OrderTable admin={true} orders={orders} />
+			</div>
+
+			{totalPages > 1 && (
+				<Pagination
+					route="admin/orders"
+					page={page}
+					totalPages={totalPages}
+				/>
+			)}
+		</>
+	);
 }
